baacb566899b0b080894780f4b3e743295cf6bc1
Acid <masatozhongcun808@gmail.com>
2025-01-18 13:48:22 +0900

    Initial \

---
+++ TNKMNK/Kernel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Sys = Cosmos.System;
+
+namespace TNKMNK
+{
+    public class Kernel : Sys.Kernel
+    {
+
+        protected override void BeforeRun()
+        {
+            Console.Clear();
+            Console.WriteLine("TNKMNK KERNEL");
+        }
+
+        protected override void Run()
+        {
+            Console.Write("Input: ");
+            var input = Console.ReadLine();
+        }
+    }
+}
